e373b7007b45cdbcd4326584bd9735785e6d9d38
Luigi Marino <emails@luigi-marino.com>
2022-08-15 10:45:48 +0000

    Get DNS Records for luigi-marino.com

--- External_IP_Check.py
+++ External_IP_Check.py
@@ -1,4 +1,5 @@
 from requests import get
+import secret
 
 # Current saved external IP Address
 ip_saved = '1.1.1.1' 
@@ -7,7 +8,10 @@ ip_saved = '1.1.1.1'
 ip_check = get('https://api.ipify.org').text
 
 # GoDaddy API call
+headers = {'Authorization': 'sso-key {}:{}'.format(secret.api_key, secret.secret_key)}
+url = 'https://api.godaddy.com/v1/domains/luigi-marino.com/records/A/%40'
 
+test = get(url, params = {'domain':'luigi-marino.com'}, headers=headers).text
 
 # Check if IP addresses match
 if ip_check != ip_saved:
@@ -15,3 +19,5 @@ if ip_check != ip_saved:
 
 # Test Prints
 print(ip_check)
+print(secret.api_key)
+print(test)
