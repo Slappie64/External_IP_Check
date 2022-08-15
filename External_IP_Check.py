fa521a8741b52eea681b9e4b5368a662c3d15d9c
Luigi Marino <emails@luigi-marino.com>
2022-08-15 11:27:05 +0000

    Documentation for each part of script

--- External_IP_Check.py
+++ External_IP_Check.py
@@ -1,3 +1,4 @@
+# Imports
 from requests import get
 import secret
 import json
@@ -12,19 +13,24 @@ ip_check = get('https://api.ipify.org').text
 headers = {'Authorization': 'sso-key {}:{}'.format(secret.api_key, secret.secret_key)}
 url = 'https://api.godaddy.com/v1/domains/luigi-marino.com/records/A/%40'
 
-test = get(url, params = {'domain':'luigi-marino.com'}, headers=headers).text
+# Get raw DNS information from GoDaddy API
+dns_raw = get(url, params = {'domain':'luigi-marino.com'}, headers=headers).text
 
-# Take in information from GoDaddy API and make it usable
-test = test.replace('[','')
-test = test.replace(']','')
-api_json = json.loads(test)
+# Take in raw information from GoDaddy API and turn it into a useable json string
+dns_raw = dns_raw.replace('[','')
+dns_raw = dns_raw.replace(']','')
+dns_json = json.loads(dns_raw)
 
+# Get the current name from the json data
 current_ip = api_json['data']
 
+# Check current external IP against GoDaddy DNS Records
 if ip_check != current_ip:
     print('Nope')
 else:
     print('Yep')
+
+    
 # Test Prints
 #print(ip_check)
 #print(secret.api_key)
