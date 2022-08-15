dce23845af1a8e66b3a4fbe77452c26f1dc01c4e
Luigi Marino <emails@luigi-marino.com>
2022-08-15 10:22:13 +0000

    Request external IP and check against current

---
+++ External_IP_Check.py
@@ -0,0 +1,13 @@
+from requests import get
+
+# Current saved external IP Address
+ip_saved = '1.1.1.1' 
+
+# Get current external IP Address
+ip_check = get('https://api.ipify.org').text
+
+if ip_check != ip_saved:
+    print('nope')
+
+# Test Prints
+print(ip_check)
