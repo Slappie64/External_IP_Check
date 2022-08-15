05220979d7964175c24ee59eeb551c0a920da779
Luigi Marino <emails@luigi-marino.com>
2022-08-15 13:08:10 +0000

    Created client session, pulled DNS records and get external IP from ipify api

--- External_IP_Check.py
+++ External_IP_Check.py
@@ -1,8 +1,23 @@
 import secret
 from godaddypy import Client, Account
+from requests import get
 
-account = Account(api_key = secret.public_key, api_secret = secret.secret_key)
-
+# Configure account details
+account = Account(api_key=secret.public_key, api_secret=secret.secret_key)
 client = Client(account)
 
-print(client.get_domains())
+# Get current DNS record and assign to dns_ip
+dns_ip = client.get_records('luigi-marino.com', record_type='A')[0]['data']
+
+# Get current external IP address and assign to ext_ip
+ext_ip = get('https://api.ipify.org').text
+
+# Check whether the two IP addresses match
+if ext_ip != dns_ip:
+    print('Nope')
+
+# Test Outputs
+# print(client.get_domains())
+print(client.get_records('luigi-marino.com', record_type='A'))
+print('DNS IP: '+dns_ip)
+print('EXT IP: '+ext_ip)
