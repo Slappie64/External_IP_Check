7659d3ec0a018bcea11300b7fdce6199d0b3247d
Luigi Marino <emails@luigi-marino.com>
2022-08-15 10:29:23 +0000

    Begun work on adding GoDaddy API integration

--- External_IP_Check.py
+++ External_IP_Check.py
@@ -6,6 +6,10 @@ ip_saved = '1.1.1.1'
 # Get current external IP Address
 ip_check = get('https://api.ipify.org').text
 
+# GoDaddy API call
+
+
+# Check if IP addresses match
 if ip_check != ip_saved:
     print('nope')
 
